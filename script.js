3d8a12e6c7946e3bf343fe7d73c8492e7eb265fe
muzanydpr78-glitch <muzanydpr.78@gmail.com>
2026-09-11 12:54:14 +0700

    Refactor script.js for improved navigation and auth
    Updated API URL and refactored event listeners for navigation and authentication flows.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     // --- Konfigurasi URL Backend GAS ---
-    const API_URL = "https://script.google.com/macros/s/AKfycbwVq5FGY_40C03WxwV4arPwC_EuHaUtLNjqsaNbJBJSybzcZ8QWZ03d5AEpD3B9ui8/exec";
+    const API_URL = "https://script.google.com/macros/s/AKfycbyN-rVJ7BP1cKiuyhbA300Np8ll4PV-rR0UAJhXcwI2qXyU5rs3y9fV019PsBoBt_S4/exec";
 
     // --- Elements ---
     const landingPage = document.getElementById('landing-page');
